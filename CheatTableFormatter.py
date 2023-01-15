c83939c6b96ebd5930148dbd3bc1bd007e95b358
Jeremy Peters <contact@celltec.dev>
2023-01-15 14:11:23 +0100

    Add print [with bug]

--- CheatTableFormatter.py
+++ CheatTableFormatter.py
@@ -6,31 +6,32 @@ import xml.etree.ElementTree as ET
 @dataclass
 class CheatEntry:
     description: str = 'N/A'
-    childreen: list = field(default_factory=list)
+    children: list = field(default_factory=list)
 
 def parse_cheat_entries(et_entries: ET.Element) -> list[CheatEntry]:
     entries = []
     for et_entry in et_entries:
         entry = CheatEntry()
-        for et_element in et_entry:
-            match et_element.tag:
-                case 'Description':
-                    entry.description = et_element.text.replace('"', '')
-                case 'CheatEntries':
-                    entry.childreen = parse_cheat_entries(et_element)
+
+        if (description := et_entry.find('Description')) is not None:
+            entry.description = description.text.replace('"', '')
+
+        if (children := et_entry.find('CheatEntries')) is not None:
+            entry.children = parse_cheat_entries(children)
+
         entries.append(entry)
     return entries
 
-def print_cheat_table(table: list[CheatEntry]):
-    # TODO
-
-    test = [CheatEntry(description='DEBUG', childreen=[]),
-            CheatEntry(description='SCRIPTS ▼',
-                       childreen=[CheatEntry(description='INTERFACE', childreen=[]),
-                                  CheatEntry(description='MATCH ▼',
-                                             childreen=[CheatEntry(description='target',
-                                                                   childreen=[])])])]
-    assert table == test
+def print_cheat_entry(entry: CheatEntry, depth: int = 0):
+    print(entry.description)
+    if entry.children:
+        depth += 1
+        space = '│  ' * (depth - 1)
+        for child in entry.children[:-1]:
+            print(f'{space}├─ ', end='')
+            print_cheat_entry(child, depth)
+        print(f'{space}└─ ', end='')
+        print_cheat_entry(entry.children[-1], depth)
 
 def main():
     parser = ArgumentParser(description='Print out an overview of a given cheat table')
@@ -42,7 +43,9 @@ def main():
 
     try:
         if entries := ET.parse(args.file).getroot().find('CheatEntries'):
-            print_cheat_table(parse_cheat_entries(entries))
+            cheat_table = parse_cheat_entries(entries)
+            for entry in cheat_table:
+                print_cheat_entry(entry)
     except ET.ParseError:
         print(f'"{args.file}" is corrupted')
 
